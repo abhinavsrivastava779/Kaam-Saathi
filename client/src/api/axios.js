db6832063c57d5800918c927c215b5177d1c9564
Abhinav Srivastava <abhinavsrivastava779@gmail.com>
2026-08-17 16:07:53 +0530

    Fix frontend API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,21 +1,24 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('kaam_saathi_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('kaam_saathi_token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
